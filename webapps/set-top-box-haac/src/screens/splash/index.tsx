e737a686c77abdf786858385a49089fdc5ca0546
David Cantelar <david.cantelar@telefonica.com>
2020-09-28 15:54:37 +0200

    feat: add custom splash

--- webapps/set-top-box-haac/src/screens/splash/index.tsx
+++ webapps/set-top-box-haac/src/screens/splash/index.tsx
@@ -1,16 +1,31 @@
 import React, { useEffect } from 'react';
-import { Preloadable, useAura } from '@telefonica/la-web-sdk';
+import { AuraCommands, Preloadable, useAura, useBackground } from '@telefonica/la-web-sdk';
+
 import { Intent } from '../../../../../dialogs/src/models';
+import './Splash.scss';
+import splashBg from '../../assets/manbat-splash.svg';
 
 const SplashScreen: React.FC<Preloadable> = ({ onReady }: Preloadable) => {
-    const { sendCommand } = useAura();
+    const background = useBackground(); // to be able to show a background
+    const { sendCommand } = useAura(); // to send commands to aura
 
     useEffect(() => {
-        onReady();
-        sendCommand({ intent: Intent.HOME, entities: [] });
+        // the method setBackground is used to use the svg as the background of the screen
+        background.setBackground(splashBg).then((img: any) => {
+            // if for example the svg could not be set as the background,
+            // a background color is set
+            if (!img) {
+                background.setBackgroundColor('black');
+            }
+            // navigate to home screen
+            sendCommand(AuraCommands.getAuraCommand(Intent.HOME));
+            // with this method we notify the sdk that this component is ready to be shown
+            onReady();
+        });
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [onReady, sendCommand]);
 
-    return <div className="SplashScreen">SPLASH</div>;
+    return <div id="splash">Welcome to the Tour of Heroes!</div>;
 };
 
 export default SplashScreen;
