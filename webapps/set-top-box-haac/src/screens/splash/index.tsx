d301cd36b4e63fce8b9771e5337d1e0057887f52
David Cantelar <david.cantelar@telefonica.com>
2020-10-08 19:40:17 +0200

    feat: add cool font for toh

--- webapps/set-top-box-haac/src/screens/splash/index.tsx
+++ webapps/set-top-box-haac/src/screens/splash/index.tsx
@@ -6,24 +6,23 @@ import './Splash.scss';
 import splashBg from '../../assets/manbat-splash.svg';
 
 const SplashScreen: React.FC<Preloadable> = ({ onReady }: Preloadable) => {
-    const background = useBackground(); // to be able to show a background
+    const { setBackground, setBackgroundColor } = useBackground(); // to be able to show a background
     const { sendCommand } = useAura(); // to send commands to aura
 
     useEffect(() => {
         // the method setBackground is used to use the svg as the background of the screen
-        background.setBackground(splashBg).then((img: HTMLImageElement) => {
+        setBackground(splashBg).then((img: HTMLImageElement) => {
             // if for example the svg could not be set as the background,
             // a background color is set
             if (!img) {
-                background.setBackgroundColor('black');
+                setBackgroundColor('black');
             }
             // navigate to home screen
             sendCommand(AuraCommands.getAuraCommand(Intent.HOME));
             // with this method we notify the sdk that this component is ready to be shown
             onReady();
         });
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [onReady, sendCommand]);
+    }, [setBackground, setBackgroundColor, onReady, sendCommand]);
 
     return <div id="splash">Welcome to the Tour of Heroes!</div>;
 };
